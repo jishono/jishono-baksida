ac761fe4a86fbd3a6642df47a26abed46611bc58
Per Gunnar Paulsen <pergpau@gmail.com>
2020-06-03 22:40:48 +0200

    fixed env

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "./config.js";
 
 export default axios.create({
-  baseURL: config.apiurl,
+  baseURL: process.env.VUE_APP_NODE_HOST,
   headers: {
     "Content-type": "application/json",
     'Authorization': localStorage.getItem('jwt') || ''
